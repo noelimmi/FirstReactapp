3a4009da86842439a312ac058f9f0e3f6a42e9e6
noelimmi <noelimmanuel1998@gmail.com>
2019-01-05 01:14:03 +0530

    React TODO app

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is version 1.0 React</p>
+    </React.Fragment>
+  )
+}
+
+export default About;
